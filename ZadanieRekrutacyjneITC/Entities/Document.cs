55c528dc98009b219b30582c7021b3f886077f4f
Francuz94 <Forward552@gmail.com>
2022-09-05 11:20:06 +0200

    Dodaj pliki projektów.

---
+++ ZadanieRekrutacyjneITC/Entities/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieRekrutacyjneITC.Entities
+{
+    public class Document
+    {
+        public int ClientNumber { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime EditDate { get; set; }
+
+
+    }
+}
